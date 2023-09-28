6934002df515ce8b2618d0f7099ab014ad9b17ef
Bohdan <nabogch@gmail.com>
2023-09-28 19:04:34 +0300

    Add an example with TinyMCE

---
+++ examples/wproofreader-sdk-vue-tinymce/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import WProofreaderSDK from '@webspellchecker/wproofreader-sdk-js';
+import Editor from './components/Editor.vue'
+
+WProofreaderSDK.configure({
+	autoSearch: true,
+	autoDestroy: true,
+	enforceAI: true,
+	serviceId: 'your-service-id'
+});
+</script>
+
+<template>
+	<main>
+		<h1>WProofreader SDK & TinyMCE demo in Vue.js</h1>
+		<Editor />
+	</main>
+</template>
